a69d48366bac61ad9f58cc980e96aa9aa6eb0f55
FateevMaxim <fateev.maxim@gmail.com>
2021-07-29 00:33:44 +0600

    What's app button

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -508,7 +508,21 @@
 <!--Main JS (Common Activation Codes)-->
 <script src="{{asset('js/main.js')}}"></script>
 <!-- <script src="assets/js/main.min.js"></script> -->
-
+<!-- GetButton.io widget -->
+<script type="text/javascript">
+    (function () {
+        var options = {
+            whatsapp: "+77003033360", // WhatsApp number
+            call_to_action: "Чем мы можем Вам помочь?", // Call to action
+            position: "right", // Position may be 'right' or 'left'
+        };
+        var proto = document.location.protocol, host = "getbutton.io", url = proto + "//static." + host;
+        var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
+        s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
+        var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+    })();
+</script>
+<!-- /GetButton.io widget -->
 @yield("customJs")
 
 </body>
