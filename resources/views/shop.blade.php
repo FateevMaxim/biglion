2614fea76cc3d1d1be7e4a7f36d1ed8721cf9b4c
FateevMaxim <fateev.maxim@gmail.com>
2021-08-24 00:44:31 +0600

    First attempt to optimize

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -7,7 +7,8 @@
     <meta property="og:title" content="Купить спортивное питание от Big Lion, лучшая цена в Алматы, Казахстане!">
 @endsection
 @section('style')
-    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+    <link rel="preload" href="{{asset('css/style.css')}}" as="style" onload="this.onload=null;this.rel='stylesheet'">
+    <noscript><link rel="stylesheet" href="{{asset('css/style.css')}}"></noscript>
 @endsection
 @section('content')
     <main class="main-content">
